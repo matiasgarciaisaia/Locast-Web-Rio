f7ac9df43a8a47f8a946c08904f04b6c807d997e
Martin Verzilli <mverzilli@manas.com.ar>
2013-05-15 18:14:55 -0300

    Added static context processor

--- settings.py
+++ settings.py
@@ -113,6 +113,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
     'locast.context_processors.settings_variables'
 )
