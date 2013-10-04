b55669adb9a0b246de2623ab6f25c09a028994d3
Martin Verzilli <mverzilli@manas.com.ar>
2013-10-04 13:25:04 -0300

    Ensure can_post_to_social_networks is set

--- travels/forms.py
+++ travels/forms.py
@@ -70,6 +70,9 @@ class RegisterForm(forms.Form):
         u.profile = cleaned_data.get('profile')
         u.personal_url = cleaned_data.get('personal_url')
 
+        #Fixes an issue which happens on register
+        u.can_post_to_social_networks = False
+
         u.save()
 
         # lc stands for locast!!
